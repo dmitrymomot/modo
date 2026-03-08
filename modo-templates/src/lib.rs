9756138d7c799a52900342a3c36287364e1fa4cd
Dmytro Momot <mail@dmomot.com>
2026-03-08 23:06:35 +0700

    feat(modo-templates): implement context_layer middleware

--- modo-templates/src/lib.rs
+++ modo-templates/src/lib.rs
@@ -2,12 +2,14 @@ pub mod config;
 pub mod context;
 pub mod engine;
 pub mod error;
+pub mod middleware;
 pub mod view;
 
 pub use config::TemplateConfig;
 pub use context::TemplateContext;
 pub use engine::{TemplateEngine, engine};
 pub use error::TemplateError;
+pub use middleware::ContextLayer;
 pub use view::View;
 
 // Re-export macro
