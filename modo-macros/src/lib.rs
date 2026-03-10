9115e4698e9c2791dd1b6927d644e88a3ddafae4
Dmytro Momot <mail@dmomot.com>
2026-03-10 16:58:02 +0700

    feat(macros): add #[template_function] proc macro

--- modo-macros/src/lib.rs
+++ modo-macros/src/lib.rs
@@ -7,6 +7,7 @@ mod middleware;
 mod module;
 mod sanitize;
 mod t_macro;
+mod template_function;
 mod validate;
 mod view;
 
@@ -70,6 +71,14 @@ pub fn t(input: TokenStream) -> TokenStream {
 #[proc_macro_attribute]
 pub fn view(attr: TokenStream, item: TokenStream) -> TokenStream {
     view::expand(attr.into(), item.into())
+        .unwrap_or_else(|e| e.to_compile_error())
+        .into()
+}
+
+/// Auto-register a function as a MiniJinja template function.
+#[proc_macro_attribute]
+pub fn template_function(attr: TokenStream, item: TokenStream) -> TokenStream {
+    template_function::expand(attr.into(), item.into())
         .unwrap_or_else(|e| e.to_compile_error())
         .into()
 }
